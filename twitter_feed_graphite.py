6cc4cdeff344f4e984339c73052ea2e9ae77a37b
Justin Knoll <justin.knoll@gmail.com>
2017-01-09 14:45:08 -0800

    Added rate limit backoff, multiple metrics, refactoring

--- twitter_feed_graphite.py
+++ twitter_feed_graphite.py
@@ -6,13 +6,24 @@ from config import config
 api = twitter.Api(config['consumer_key'],
                   config['consumer_secret'],
                   config['access_token_key'],
-                  config['access_token_secret'])
-followers = api.GetFollowers()
-follower_count = len(followers)
-print(follower_count)
+                  config['access_token_secret'],
+                  sleep_on_rate_limit=True)
 
-message = config['graphite_api_key'] + ".personal.twitter.followers %s\n" % follower_count
+stats = {}
 
-conn = socket.create_connection(("carbon.hostedgraphite.com", 2003))
-conn.send(message)
-conn.close()
+stats['followers'] = len(api.GetFollowers())
+stats['friends'] = len(api.GetFriends())
+stats['mentions'] = len(api.GetMentions())
+stats['retweets'] = len(api.GetRetweetsOfMe())
+stats['favorites'] = len(api.GetFavorites())
+
+def send_message(stat_name, value):
+    message = config['graphite_api_key'] + "." + stat_name + " %s\n" % value
+    print message
+    conn = socket.create_connection(("carbon.hostedgraphite.com", 2003))
+    conn.send(message)
+    conn.close()
+
+for key in stats:
+    print(key + ": " + str(stats[key]))
+    send_message("personal.twitter." + key, stats[key])
